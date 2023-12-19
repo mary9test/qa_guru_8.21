e91fef70d462a2accd5adc13cc110c554b24b661
maria.m <maryqatest9@gmail.com>
2023-12-19 19:52:35 +0300

    Добавлен тест на android

--- test_android.py
+++ test_android.py
@@ -14,7 +14,7 @@ def test_search_appium():
         results.first.should(have.text('Appium'))
 
 
-def test_search_python():
+def test_search_pytest():
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Pytest')
