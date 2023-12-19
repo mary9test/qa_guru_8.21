fd03014a4d74a0ed0590ba0ac088b4fd52848bb8
maria.m <maryqatest9@gmail.com>
2023-12-19 19:35:57 +0300

    Добавлен тест на android

---
+++ conftest.py
@@ -0,0 +1,42 @@
+import pytest
+from appium.options.android import UiAutomator2Options
+from selene import browser
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def mobile_management():
+    options = UiAutomator2Options().load_capabilities({
+        # Specify device and os_version for testing
+        # "platformName": "android",
+        "platformVersion": "9.0",
+        "deviceName": "Google Pixel 3",
+
+        # Set URL of the application under test
+        "app": "bs://sample.app",
+
+        # Set other BrowserStack capabilities
+        'bstack:options': {
+            "projectName": "First Python project",
+            "buildName": "browserstack-build-1",
+            "sessionName": "BStack first_test",
+
+            # Set your access credentials
+            "userName": os.getenv('USER_NAME'),
+            "accessKey": os.getenv('ACCESS_KEY')
+        }
+    })
+
+    # browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+    browser.config.driver_remote_url = os.getenv('REMOTE_BROWSER_URL')
+    browser.config.driver_options = options
+
+    browser.config.timeout = float(os.getenv('timeout', '10.0'))
+
+    yield
+
+    browser.quit()
